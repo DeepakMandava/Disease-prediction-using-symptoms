d050f2e68e07638af84edd4ed9bf75232b8c09ca
Deepak Mandava <84240946+DeepakMandava@users.noreply.github.com>
2022-04-30 11:43:46 +0530

    Update operations.py

--- operations.py
+++ operations.py
@@ -3,7 +3,6 @@ def compile_model():
     #importing required modules and libraries.
     import numpy as np
     import pandas as pd
-    import matplotlib.pyplot as plt
     from sklearn.model_selection import train_test_split
     from sklearn.ensemble import RandomForestClassifier
     from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
